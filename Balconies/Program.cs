6adaf7e3adbad4bd7d303f2177d28738ab6f35dc
jakub-hemerka <jhemi@outlook.cz>
2023-07-21 18:40:18 +0200

    Added second project

---
+++ Balconies/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] firstBalcony = Console.ReadLine()!.Split(',');
+        string[] secondBalcony = Console.ReadLine()!.Split(',');
+
+        int aparmentA = CalculateArea(firstBalcony);
+        int aparmentB = CalculateArea(secondBalcony);
+
+        if (aparmentA > aparmentB)
+        {
+            Console.Write("Apartment A");
+            return;
+        }
+
+        Console.Write("Apartment B");
+    }
+
+    private static int CalculateArea(string[] dimensions)
+    {
+        if (dimensions.Length == 2 && int.TryParse(dimensions[0], out int width) && int.TryParse(dimensions[1], out int heigth))
+        {
+            return width * heigth;
+        }
+
+        return 0;
+    }
+}
