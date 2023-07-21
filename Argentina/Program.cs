6934b033574b45d1e85280fd36f3f478c8054ce3
jakub-hemerka <jhemi@outlook.cz>
2023-07-21 18:32:33 +0200

    Add project files.

---
+++ Argentina/Program.cs
@@ -0,0 +1,13 @@
+﻿string priceInPesosAsString = Console.ReadLine()!;
+string priceInDollarsAsString = Console.ReadLine()!;
+
+if (int.TryParse(priceInPesosAsString, out int priceInPesos) && int.TryParse(priceInDollarsAsString, out int priceInDollars))
+{
+    if (priceInDollars * 50.0 < priceInPesos)
+    {
+        Console.Write("Dollars");
+        return;
+    }
+
+    Console.Write("Pesos");
+}
